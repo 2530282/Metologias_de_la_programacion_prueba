ba6c11421d1098db834c7c10207d81aaa199c304
2530282 <2530282@upv.edu.mx>
2025-10-27 09:47:25 -0600

    Ejemplo

---
+++ src/understanding_variables.py
@@ -0,0 +1,31 @@
+message = "Viva Mexico!"
+another_message = 'Hello, World!'
+
+print(message)
+print(another_message)
+
+print(message, another_message, message)
+print(another_message, message)
+
+print(message)
+
+"""
+
+Los nombres de variables en Python deben nombrarse solo con:
+
+    -letras, números y guión bajo (espacios)
+    -deben comenzar con una letra o con guión bajo, pero NO con número:
+        ejemplo correcto: mensaje_1 ( :) )
+        ejemplo incorrecto: 1_mensaje
+    -no utilizar espacios para separar palabras en los nombres de las variables
+    -no utilizar palabras reservadas de Python para nombrar variables o archivos
+    -deben ser cortos, pero descriptivos
+    -deben ser en inglés
+    -nombres de las variables en minúsculas 
+
+
+"""
+
+emiliano_message = "Hello from Emiliano!"
+print(emiliano_message)
+
