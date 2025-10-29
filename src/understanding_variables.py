c4f22469f1e50071764ba5333caf13c2b9ce0ecb
2530282 <2530282@upv.edu.mx>
2025-10-29 09:34:19 -0600

    feat: traceback

--- src/understanding_variables.py
+++ src/understanding_variables.py
@@ -26,6 +26,21 @@ Los nombres de variables en Python deben nombrarse solo con:
 
 """
 
-emiliano_message = "Hello from Emiliano!"
+emiliano_message = "Hola, soy Charly y estoy aprendiendo Python!"
 print(emiliano_message)
+print(emiliano_mesage)
 
+"""
+traceback : Es un registro de donde el interpetre tuvo ploblemas al intentar ejecutar el código.
+
+    Ejemplo:
+
+    Traceback (most recent call last):
+      File "C:/Users/ebarc/projects/Metologias_de_la_programacion_prueba/src/understanding_variables.py", line 31, in <module>
+        print(emiliano_mesage)
+              ^^^^^^^^^^^^^^^
+        NameError: name 'emiliano_mesage' is not defined. Did you mean: 'emiliano_message'?
+
+    NameError: Significa que olvidamos establecer el valor de una variable antes de utilizar o cometimos un error al ingresar el nombre de la variable.
+
+"""
